0e9e7c9d786e7cbde39379aaaabe214c4b480476
kiptanui boaz <boaserem022@gmail.com>
2023-01-30 20:14:49 +0300

    Added persist functionality

--- server/index.js
+++ server/index.js
@@ -21,22 +21,69 @@ app.get("/", (req, res) => {
 
 app.get("/books", (req, res) => {
     const queryAllBooks = "SELECT * FROM books";
-        db.query(queryAllBooks, (err, data)=>{
-            if(err){
-                return res.status(500).json({"message": err.message});
-            }else{
-                return res.json(data);
-            }
-        }); 
+
+    db.query(queryAllBooks, (err, data)=>{
+        if(err){
+            return res.status(500).json({"message": err.message});
+        }else{
+            return res.json(data);
+        }
+    }); 
 })
 
+app.get("/books/:id", (req, res) => {
+    const bookId = req.params.id;
+    const queryBook = "SELECT * FROM books  WHERE id = ?";
+    
+    db.query(queryBook,[bookId], (err, data)=>{
+        if(err){
+            console.log(err);
+            return res.status(500).json({"message": err.message});
+        }else{
+
+            return res.json(data[0]);
+        }
+    }); 
+})
+
+
 app.post("/books",(req,res)=>{
-    const addBooksQuery = "INSERT INTO books (`titile`,`description`,`cover`) VALUES(?)"
+    const addBooksQuery = "INSERT INTO books (`title`,`description`,`cover`,`price`) VALUES(?)"
     const values = Object.values(req.body);
 
     db.query(addBooksQuery,[values],(err,data)=>{
         if (err) return res.json(err.message);
         return res.json(data)
+
+    })
+})
+
+app.delete("/books/:id", (req,res)=>{
+    const bookId = req.params.id;
+    const deleteQuery = "DELETE FROM books WHERE id = ?"
+
+    db.query(deleteQuery, [bookId], (err,data)=>{
+        if (err) return res.json(err.message);
+        return res.json(data)
+
+    })
+})
+
+app.put("/books/:id", (req,res)=>{
+    const bookId = req.params.id;
+    const updateQuery = "UPDATE books SET `title`= ?, `description` = ?,  `price` = ?,  `cover` = ? WHERE  id = ? ";
+
+    const values = Object.values(req.body)
+    
+
+    db.query(updateQuery, [...values, bookId], (err,data)=>{
+        if (err){
+            console.log(err);
+            return res.json(err.message);
+        }else{
+            return res.json(data);
+        }
+        
 
     })
 })
